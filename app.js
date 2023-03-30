00e89a2582f553b65694ea88344a295e3bc89a8f
Bourskill <criyeak@gmail.com>
2023-03-30 13:04:24 -0500

    master

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const carousel = document.querySelector('.carousel');
 
-var flkty = new Flickity(carousel, {
+var flkty = new Flickity('.carousel', {
     // opciones generales
     cellAlign: 'left',
     contain: true,
@@ -15,12 +14,3 @@ flkty.on('dragEnd', function () {
 });
 
 
-
-carousel.addEventListener('scroll', () => {
-    flkty.slides.forEach((slide, i) => {
-        const image = carousel.querySelector(`[data-index="${i}"] img`);
-        const x = (slide.target + flkty.x) * -1;
-        let percent = Math.max(0, Math.min(1, (x + slide.width) / (flkty.viewport.offsetWidth + slide.width)));
-        image.style.opacity = percent;
-    });
-});
