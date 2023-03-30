5563f5e63f8f636794b1e539cd64586769af6c7b
Bourskill <criyeak@gmail.com>
2023-03-30 16:43:15 -0500

    master

--- app.js
+++ app.js
@@ -1,67 +1,56 @@
-// mover todo el código de inicialización del carrusel fuera de la función window.onload
-var flickity  = new Flickity('.carousel', {
-    // opciones generales
-    cellAlign: 'left',
-    contain: true,
-    wrapAround: true,
-    autoPlay: 4000, // cambia de imagen cada 4 segundos
-    pauseAutoPlayOnHover: true, // detiene el cambio de imagen cuando el mouse está encima del carrusel
-    fade: true,
-    adaptiveHeight: true,
-    lazyLoad: true,
-    setGallerySize: false,
-    opacity: true
+const images = document.querySelectorAll('.carousel img');
+const containers = document.querySelectorAll('.carousel .container-cell');
+const flickity = new Flickity('.carousel', {
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  autoPlay: 4000,
+  pauseAutoPlayOnHover: true,
+  fade: true,
+  adaptiveHeight: true,
+  lazyLoad: true,
+  setGallerySize: false,
+  opacity: true
+});
+
+function updateImageOpacity() {
+  flickity.slides.forEach(function(slide, i) {
+    const container = slide.cells[0].element;
+    const imageWidth = container.querySelector('img').offsetWidth;
+    const slideWidth = flickity.slidesWidth;
+    const slidePosition = slide.target + flickity.x;
+    const opacity = 1 - Math.abs(slidePosition) / (slideWidth / 2);
+
+    container.style.opacity = Math.max(0, Math.min(1, opacity));
   });
-  
-
-  
- 
-// Actualizar la opacidad de las imágenes en función de su posición en el carrusel
-flickity.on('scroll', function() {
-    flickity.slides.forEach(function(slide, i) {
-      var image = slide.cells[0].element.querySelector('img');
-      var imageWidth = image.offsetWidth;
-      var slideWidth = flickity.slidesWidth;
-      var slidePosition = slide.target + flickity.x;
-      var opacity = 1 - Math.abs(slidePosition) / (slideWidth / 2);
-      opacity = Math.max(0, opacity);
-      opacity = Math.min(1, opacity);
-      image.style.opacity = opacity;
-    });
-  });
-  
-  
-
-  function handleCarousel() {
-    const images = document.querySelectorAll('.carousel img');
-
-    images.forEach(img => {
-        const boundingRect = img.getBoundingClientRect();
-        const percentVisible = boundingRect.x / window.innerWidth;
-
-        img.style.opacity = 1 - percentVisible;
-    });
 }
 
 function preloadImages() {
-    const images = document.querySelectorAll('.carousel img');
-    images.forEach(img => {
-      const src = img.getAttribute('src');
-      const imgElement = new Image();
-      imgElement.src = src;
-    });
-  }
-  
-  preloadImages();
-  
-
-  
-  
-  
-
-  
-  // reinicia el cambio de imagen cuando se termina de interactuar con el carrusel
-  flickity.on('dragEnd', function () {
-    flickity.playPlayer();
+  images.forEach(function(image) {
+    const src = image.getAttribute('src');
+    const imgElement = new Image();
+    imgElement.src = src;
   });
-  
+}
+
+function updateContainerOpacity() {
+  containers.forEach(function(container) {
+    const boundingRect = container.getBoundingClientRect();
+    const percentVisible = boundingRect.x / window.innerWidth;
+    container.style.opacity = 1 - percentVisible;
+  });
+}
+
+flickity.on('scroll', updateImageOpacity);
+flickity.on('dragEnd', function () {
+  flickity.playPlayer();
+});
+
+window.addEventListener('load', function() {
+  preloadImages();
+  updateContainerOpacity();
+});
+
+window.addEventListener('resize', function() {
+  updateContainerOpacity();
+});
