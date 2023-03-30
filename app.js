9b18f809199d9d826874b3d7d3972307c1613ecc
Bourskill <criyeak@gmail.com>
2023-03-30 13:22:53 -0500

    master

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-const flkty = new Flickity('.carousel', {
+// mover todo el código de inicialización del carrusel fuera de la función window.onload
+var flkty = new Flickity('.carousel', {
     // opciones generales
     cellAlign: 'left',
     contain: true,
     wrapAround: true,
     autoPlay: 4000, // cambia de imagen cada 4 segundos
     pauseAutoPlayOnHover: true, // detiene el cambio de imagen cuando el mouse está encima del carrusel
+    fade: true,
+    adaptiveHeight: true,
     lazyLoad: true,
-    setGallerySize: false
+    setGallerySize: false,
+    opacity: true
   });
   
-  flkty.on('scroll', () => {
-    const visibleIndex = flkty.selectedIndex;
-    const cells = flkty.cells;
-  
-    cells.forEach((cell, index) => {
-      const img = cell.element.querySelector('img');
-  
-      if (index === visibleIndex) {
-        img.style.opacity = 1;
-      } else {
-        const distance = Math.abs(index - visibleIndex);
-  
-        if (distance <= 1) {
-          img.style.opacity = 0.7 + 0.3 * (1 - distance);
-        } else {
-          img.style.opacity = 0.3;
-        }
-      }
+  // Actualizar la opacidad de las imágenes en función de su posición en el carrusel
+flickity.on('scroll', function() {
+    flickity.slides.forEach(function(slide) {
+      var image = slide.cells[0].element;
+      var imageWidth = image.offsetWidth;
+      var slideWidth = flickity.slidesWidth;
+      var slidePosition = slide.target + flickity.x;
+      var opacity = 1 - Math.abs(slidePosition) / (slideWidth / 2);
+      opacity = Math.max(0, opacity);
+      opacity = Math.min(1, opacity);
+      image.style.opacity = opacity;
     });
+  });
+
+  
+  // reinicia el cambio de imagen cuando se termina de interactuar con el carrusel
+  flkty.on('dragEnd', function () {
+    flkty.playPlayer();
   });
   
