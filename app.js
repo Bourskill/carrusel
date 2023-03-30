b3a23a0878d123e3c11b300cfc86caa3aeaa755d
Bourskill <criyeak@gmail.com>
2023-03-30 13:11:06 -0500

    master

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var flkty = new Flickity('.carousel', {
     // opciones generales
     cellAlign: 'left',
@@ -6,11 +5,25 @@ var flkty = new Flickity('.carousel', {
     wrapAround: true,
     autoPlay: 4000, // cambia de imagen cada 4 segundos
     pauseAutoPlayOnHover: true, // detiene el cambio de imagen cuando el mouse está encima del carrusel
-});
-
-// reinicia el cambio de imagen cuando se termina de interactuar con el carrusel
-flkty.on('dragEnd', function () {
+    fade: true,
+    adaptiveHeight: true,
+    lazyLoad: true,
+    setGallerySize: false,
+    // función que se ejecuta al desplazarse por el carrusel
+    on: {
+      scroll: () => {
+        const visibleImage = flkty.slides[flkty.selectedIndex].querySelector('img');
+        const visibleHeight = visibleImage.offsetHeight;
+        const galleryHeight = flkty.viewportHeight;
+  
+        const opacity = 1 - (galleryHeight - visibleHeight) / galleryHeight;
+        visibleImage.style.opacity = opacity;
+      }
+    }
+  });
+  
+  // reinicia el cambio de imagen cuando se termina de interactuar con el carrusel
+  flkty.on('dragEnd', function () {
     flkty.playPlayer();
-});
-
-
+  });
+  
